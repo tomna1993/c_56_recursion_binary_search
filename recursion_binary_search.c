1b2f1e00a84d6e01ac382dcb6d1ba6fb7c0bff9e
TomasNagy <tomi.nagy@outlook.com>
2023-05-31 12:36:09 +0200

    feat: add ordered list

--- recursion_binary_search.c
+++ recursion_binary_search.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
+#define ARRAY_LENGTH 11
+
 int main(void)
 {
+    int ordered_list[ARRAY_LENGTH] = {0,1,2,3,4,5,6,7,8,9,10};
+
     int number_to_find = get_int("Number: ");    
 }
