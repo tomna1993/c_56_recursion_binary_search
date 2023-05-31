0497a46270294dfeaf359ddf0a85dc4f6990d213
TomasNagy <tomi.nagy@outlook.com>
2023-05-31 12:53:01 +0200

    feat: implement binary search with recursion

--- recursion_binary_search.c
+++ recursion_binary_search.c
@@ -3,9 +3,40 @@
 
 #define ARRAY_LENGTH 11
 
+int binary_search(int sorted_array[], int start_index, int end_index, int find_number);
+
 int main(void)
 {
-    int ordered_list[ARRAY_LENGTH] = {0,1,2,3,4,5,6,7,8,9,10};
+    int ordered_list[ARRAY_LENGTH] = {0,2,4,6,8,10,12,14,17,19,30};
 
     int number_to_find = get_int("Number: ");    
+
+    int index = binary_search(ordered_list, 0, ARRAY_LENGTH - 1, number_to_find);
+
+    printf ("Found, index: %i\n", index);
+}
+
+int binary_search(int sorted_array[], int start_index, int end_index, int find_number)
+{
+    int mid = (start_index + end_index) / 2;
+
+    // Define base-case / exit condition
+    if (start_index > end_index)
+    {
+        return mid;
+    }
+
+    if (find_number == sorted_array[mid])
+    {
+        return mid;
+    }
+
+    if (find_number < sorted_array[mid])
+    {
+        return binary_search(sorted_array, start_index, mid - 1, find_number);
+    }
+    else if (find_number > sorted_array[mid])
+    {
+        return binary_search(sorted_array, mid + 1, end_index, find_number);
+    }   
 }
